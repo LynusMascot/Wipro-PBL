bf27effcda2116187050fd6a9ab3b748e2bd46b1
Rohit Goswami <lynus.mascot@gmail.com>
2018-08-25 01:43:23 -0400

    Update [ 2.5d ] Program 1.java

--- [ 2.5d ] Program 1.java
+++ [ 2.5d ] Program 1.java
@@ -1,5 +1,16 @@
 
 //Coder: Rohit Goswami
+
+/*Write a Program that accepts two Strings as command line arguments and generate the output in a specific way as given below.
+
+Example:
+
+If the two command line arguments are Wipro and Bangalore then the output generated should be Wipro Technologies Bangalore.
+
+If the command line arguments are ABC and Mumbai then the output generated should be ABC Technologies Mumbai 
+
+[Note: It is mandatory to pass two arguments in command line]
+*/
 public class Main
 {
 	public static void main(String[] args) {
