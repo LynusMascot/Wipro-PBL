268ad4474474de0b3893e86a930856c8813dddff
Rohit Goswami <lynus.mascot@gmail.com>
2018-08-25 01:42:30 -0400

    Update [ 2.5d ] Program 1.java

--- [ 2.5d ] Program 1.java
+++ [ 2.5d ] Program 1.java
@@ -1,1 +1,16 @@
 
+//Coder: Rohit Goswami
+public class Main
+{
+	public static void main(String[] args) {
+     if(args.length>0){
+         String a= args[0];
+         String b= args[1];
+         System.out.println(a+" Technologies "+b);
+     }
+     else{
+         System.out.println("Error: Not enough arguments");
+     }
+        
+	}
+}
